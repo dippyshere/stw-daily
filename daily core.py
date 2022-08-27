ff938ba7f5cd0480ce367e7ac9159795360039e6
Alex Hanson <dippyshere@gmail.com>
2022-08-27 16:29:15 +1000

    remove unused imports + PEP 8
    Signed-off-by: Alex Hanson <dippyshere@gmail.com>

--- daily core.py
+++ daily core.py
@@ -1,59 +1,45 @@
 print("Starting STW Daily")
 
-
-import asyncio
-import aiohttp
-import math
-import random
-import re
 import os
-import sys
-import psutil
-import datetime
-import time
-import stwutil as stw
-import items
-import timeit
-import json
-import logging
 
+import aiohttp
 import discord
 import discord.ext.commands as ext
 from discord.ext import tasks
-from discord import Option
-    
+
+import stwutil as stw
+
 # Compatability layer for future versions of python 3.11+ 
 try:
     import tomllib as toml
 except ModuleNotFoundError:
-    
     import tomli as toml
 
 client = ext.AutoShardedBot(command_prefix=ext.when_mentioned, case_insensitive=True)
 
+
 def load_config(config_path):
-    with open(config_path,"rb") as config_file:
+    with open(config_path, "rb") as config_file:
         config = toml.load(config_file)
         config_file.close()
 
     return config
 
+
 def main():
-    
     # Loading config file
-    config_path = "config.toml"    
+    config_path = "config.toml"
     client.config = load_config(config_path)
 
     # simple way to parse the colours from config into usable colours
     client.colours = {}
-    for name, colour  in client.config["colours"].items():
+    for name, colour in client.config["colours"].items():
         client.colours[name] = discord.Colour.from_rgb(colour[0], colour[1], colour[2])
 
     client.temp_auth = {}
     client.remove_command('help')
 
-
-    #list of extensions for stw daily to load in
+    # list of extensions for stw daily to load in
     extensions = [
         "reward",
         "help",
@@ -62,7 +48,7 @@ def main():
         "info",
         "research",
         "reminder",
-        ]
+    ]
 
     # load the extensions
     for ext in extensions:
@@ -70,19 +56,22 @@ def main():
 
     update_status.start()
     client.run(f"{os.environ['STW_DAILY_TOKEN']}")
-    
+
+
 async def create_http_session():
     return aiohttp.ClientSession()
-    
+
+
 # basic information for you <33
 @client.event
 async def on_ready():
-    client.stw_session = await create_http_session();
+    client.stw_session = await create_http_session()
     print("Started STW Daily")
 
-# simple task which updates the status ever 60 seconds to display time until next day/reset
+
+# simple task which updates the status every 60 seconds to display time until next day/reset
 @tasks.loop(seconds=60)
-async def update_status():  
+async def update_status():
     await client.wait_until_ready()
     await client.change_presence(
         activity=discord.Activity(type=discord.ActivityType.listening,
@@ -91,4 +80,3 @@ async def update_status():
 
 if __name__ == "__main__":
     main()
-    
