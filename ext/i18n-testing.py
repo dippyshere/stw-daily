5af7da6aef38bd1d60fc02b2a98fa901c91821c6
Alex Hanson <dippyshere@gmail.com>
2022-11-27 04:08:51 +1100

    update i18n test command
    Signed-off-by: Alex Hanson <dippyshere@gmail.com>

--- ext/i18n-testing.py
+++ ext/i18n-testing.py
@@ -21,36 +21,45 @@ class Internationalisation(ext.Cog):
     def __init__(self, client):
         self.client = client
 
-    async def i18n_command(self, ctx):
+    async def i18n_command(self, ctx, *args):
         """
         The main function for the i18n command.
 
         Args:
             ctx: The context of the command.
+            *args: The arguments of the command.
         """
-        user_profile = await get_user_document(self.client, ctx.author.id)
-        localisation = user_profile["profiles"][str(user_profile["global"]["selected_profile"])]["settings"]["localisation"]
+        try:
+            user_profile = await get_user_document(self.client, ctx.author.id)
+            localisation = user_profile["profiles"][str(user_profile["global"]["selected_profile"])]["settings"]["language"]
+        except KeyError:
+            localisation = "auto"
 
         embed_colour = self.client.colours["auth_white"]
         embed = discord.Embed(title=await stw.add_emoji_title(self.client, "i18n", "experimental"),
                               description=f'\u200b\n',
                               color=embed_colour)
-        try:
-            embed.description += f'\nYour chosen language: {localisation}\n'
-        except Exception as e:
-            embed.description += f'\nError getting your chosen language: ```{e}```\n'
-        try:
-            embed.description += f'\nYour language: {ctx.locale}\n'
-        except Exception as e:
-            embed.description += f'\nError getting your language: ```{e}```\n'
-        try:
-            embed.description += f'\nInteraction language: {ctx.interaction.locale}\n'
-        except Exception as e:
-            embed.description += f'\nError getting interaction language: ```{e}```\n'
-        try:
-            embed.description += f'\nGuild language: {ctx.guild.preferred_locale}\n'
-        except Exception as e:
-            embed.description += f'\nError getting guild language: ```{e}```\n'
+        if len(args) == 0 or isinstance(ctx, discord.ApplicationContext) or isinstance(ctx, discord.Message):
+            try:
+                embed.description += f'\nYour chosen language: {localisation}\n'
+            except Exception as e:
+                embed.description += f'\nError getting your chosen language: ```{e}```'
+            try:
+                embed.description += f'\nYour language: {ctx.locale}\n'
+            except Exception as e:
+                embed.description += f'\nError getting your language: ```{e}```'
+            try:
+                embed.description += f'\nInteraction language: {ctx.interaction.locale}\n'
+            except Exception as e:
+                embed.description += f'\nError getting interaction language: ```{e}```'
+            try:
+                embed.description += f'\nGuild language: {ctx.guild.preferred_locale}\n'
+            except Exception as e:
+                embed.description += f'\nError getting guild language: ```{e}```'
+            embed.description += f'\nAvailable languages:\n{stw.I18n.get_langs_str()}\n'
+            embed.description += f'\nDetermined language:\n{await stw.I18n.get_desired_lang(self.client, ctx)}\n'
+        else:
+            embed.description += f'\nThe value of {args[0]} in {await stw.I18n.get_desired_lang(self.client, ctx)} is:\n{stw.I18n.get(args[0], await stw.I18n.get_desired_lang(self.client, ctx))}\n '
 
         embed.description += f'\n\u200b'
 
@@ -69,21 +78,21 @@ class Internationalisation(ext.Cog):
         Args:
             ctx: The context of the command.
         """
-        await self.i18n_command(ctx)
+        await self.i18n_command(ctx, None)
 
     @ext.command(name='i18n',
                  aliases=[],
-                 extras={'emoji': "experimental", "args": {}, "dev": True},
+                 extras={'emoji': "experimental", "args": {"*args": "arguments to pass"}, "dev": True},
                  brief="Test internationalisation",
                  description="Test internationalisation (translation) for STW Daily")
-    async def i18n(self, ctx):
+    async def i18n(self, ctx, *args):
         """
         This function is the entry point for the i18n command when called traditionally
 
         Args:
             ctx: The context of the command.
         """
-        await self.i18n_command(ctx)
+        await self.i18n_command(ctx, *args)
 
 
 def setup(client):
